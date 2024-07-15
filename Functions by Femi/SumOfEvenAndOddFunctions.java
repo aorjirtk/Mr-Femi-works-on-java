108db684cbee6058b3084e2d18073f9a6711f10d
aorjirtk <aorjirtk2017@gmail.com>
2024-07-15 13:25:24 +0100

    Add files via upload

---
+++ Functions by Femi/SumOfEvenAndOddFunctions.java
@@ -0,0 +1,25 @@
+public class SumOfEvenAndOddFunctions{
+
+	public static int evenAndOddNumbers(){
+
+
+		if(number % 2 == 0)
+
+		evenNumber = number;
+
+		sumOfEvenNumbers = sumOfEvenNumbers+evenNumber;
+		
+		if(number % 2 == 1)
+
+		oddNumber= number;
+
+		sumOfOddNumbers = sumOfOddNumbers+oddNumber;
+		
+
+		
+	return (sumOfEvenNumbers);
+	return (sumOfOddNumbers);
+
+	}
+
+}
